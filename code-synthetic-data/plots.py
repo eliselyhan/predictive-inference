1578a2e0e7074f61db748f5c1c2643166a9ce275
elise han <lh3117@columbia.edu>
2024-05-21 09:14:22 -0400

    update mse

--- code-synthetic-data/plots.py
+++ code-synthetic-data/plots.py
@@ -12,20 +12,18 @@ def create_empty_dict(fixed_windows, num_trials):
             d['ARW'][trial] = []
     return d
 
-def plot_over_time(colors, methods, start_period, end_period, array, methods_to_plot, alpha=False, interval=False):
-
+def plot_over_time(colors, methods, start_period, end_period, array, methods_to_plot, title, alpha=False):
     fig, ax = plt.subplots(1, 1, figsize = (6,3))
     for method in methods_to_plot:
         plt.plot(array[method], color=colors[method], label=methods[method])
     if alpha:
         plt.axhline(y=1-alpha, color='k', linestyle='--', label=r'nominal')
-        plt.ylabel(r'$F_t(\widehat{q}_{t,\hat{k}})$')
-    else:
-        plt.ylabel(r'$|F_t(\widehat{q}_{t,\hat{k}})-(1-\alpha)|$')
-    if interval: 
-        plt.ylabel(r'$2\widehat{q}_{t,\hat{k}}$')
+        #plt.ylabel(r'$F_t(\widehat{q}_{t,\hat{k}})$')
+    #if interval: 
+        #plt.ylabel(r'$2\widehat{q}_{t,\hat{k}}$')
+    plt.title(title)
     plt.xlim(start_period, end_period)
-    mpl.rcParams.update({'font.size': 12})
+    mpl.rcParams.update({'font.size': 12, 'legend.fontsize': 10})    
     plt.tight_layout()
     plt.xlabel('Time')
     plt.legend()
@@ -37,16 +35,28 @@ def summarize_cdf_dict(methods, num_trials, num_periods, cdf_dict, alpha):
         for (trial, trial_coverage) in cdf_dict[key].items():
             cdf_array[i, trial, :] = trial_coverage
 
-    err_array = np.abs(cdf_array - 1 + alpha)
+    mae_array = np.abs(cdf_array - 1 + alpha)
+    mse_array = (cdf_array - 1 + alpha)**2
+
     #take average of coverage over time and trials for each method
-    mean_error = np.mean(np.mean(err_array[:, :, 100:], axis=2), axis=1)
-    se_error = np.std(np.mean(err_array[:, :, 100:], axis=2), axis=1)/np.sqrt(num_trials)
+    mae = np.mean(np.mean(mae_array[:, :, 100:], axis=2), axis=1)
+    se_ae = np.std(np.mean(mae_array[:, :, 100:], axis=2), axis=1)/np.sqrt(num_trials)
+
+    mse = np.mean(np.mean(mse_array[:, :, 100:], axis=2), axis=1)
+    se_mse = np.std(np.mean(mse_array[:, :, 100:], axis=2), axis=1)/np.sqrt(num_trials)
     #barplot errors
-    fig, ax = plt.subplots()
     colors = ['r', '#FFA500', 'tab:purple', 'tab:brown', 'tab:green', '#0096FF', 'tab:gray']
+    fig, ax = plt.subplots()
+    plt.title('Mean Absolute Error')
     for i in range(len(methods)):
-        ax.bar(methods[i], mean_error[i], color=colors[i])
-    return cdf_array, err_array, mean_error, se_error
+        ax.bar(methods[i], mae[i], color=colors[i])
+
+    fig, ax = plt.subplots()
+    plt.title('Mean Squared Error')
+    for i in range(len(methods)):
+        ax.bar(methods[i], mae[i], color=colors[i])
+
+    return cdf_array, mae_array, mae, se_ae, mse_array, mse, se_mse
 
 def summarize_interval_dict(methods, num_trials, num_periods, interval_dict):
     interval_array = np.zeros((len(methods), num_trials, num_periods))
@@ -59,6 +69,7 @@ def summarize_interval_dict(methods, num_trials, num_periods, interval_dict):
     se_interval = np.std(2 * np.mean(interval_array[:, :, 100:], axis=2), axis=1)/np.sqrt(num_trials)
     #barplot errors
     fig, ax = plt.subplots()
+    plt.title('Mean Interval Length')
     colors = ['r', '#FFA500', 'tab:purple', 'tab:brown', 'tab:green', '#0096FF', 'tab:gray']
     for i in range(len(methods)):
         ax.bar(methods[i], mean_interval[i], color=colors[i])
